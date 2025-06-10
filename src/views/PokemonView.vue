a8baefc8cb6150d214de7d406882d3658740459c
Mnauel Guzman <maguz727@gmail.com>
2025-06-09 19:51:43 -0500

    Se mejora diseño en la vista de pekemons con flexbox, se incluye botones para futura paginacion

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -6,12 +6,16 @@ import { useRoute, useRouter } from 'vue-router';
 const route = useRoute()
 const router = useRouter()
 
+const formatName = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
+
 </script>
 
 <template>
     <h1 class="mb-5">Pokémon</h1>
     <div>
-        <h4>Nombre del Pokémon: {{ route.params.pokemon }}</h4>
+        <h4>Nombre del Pokémon: {{ formatName(route.params.pokemon) }}</h4>
     </div>
 </template>
 
--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -1,30 +1,43 @@
 <script setup>
-import { RouterLink } from 'vue-router';
-import axios from 'axios'
-import { onMounted, ref } from 'vue';
+import { RouterLink } from "vue-router";
+import axios from "axios";
+import { onMounted, ref } from "vue";
 
-const pokemons = ref([])
+const pokemons = ref([]);
 
 const getData = async () => {
     try {
-        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon')
-        pokemons.value = data.results
+        const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        pokemons.value = data.results;
     } catch (error) {
-        console.log("Error al obtener la información", error)
+        console.log("Error al obtener la información", error);
     }
-}
+};
+
+const formatName = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
 
 onMounted(getData);
-
 </script>
 <template>
     <h1 class="mb-5">Pokemons</h1>
-    <div>
-        <ul>
-            <li v-for="(pokemon, index) in pokemons" :key="index">
-                <RouterLink :to="`/pokemons/${pokemon.name}`">{{ pokemon.name }}</RouterLink>
-            </li>
-        </ul>
+    <div class="d-flex flex-wrap justify-content-center">
+        <div
+            class="list-group m-1"
+            v-for="(pokemon, index) in pokemons"
+            :key="index"
+        >
+            <RouterLink
+                class="list-group-item list-group-item-action list-group-item-warning"
+                :to="`/pokemons/${pokemon.name}`"
+                >{{ formatName(pokemon.name) }}</RouterLink
+            >
+        </div>
+    </div>
+    <div class="d-flex justify-content-center mt-3 gap-2">
+        <button type="button" class="btn btn-outline-primary">Anterior</button>
+        <button type="button" class="btn btn-outline-primary">Siguiente</button>
     </div>
 </template>
 
