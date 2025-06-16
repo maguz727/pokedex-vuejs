eefbe566f8c6acd9db1d4191d413d817b11adc6c
Mnauel Guzman <maguz727@gmail.com>
2025-06-16 14:08:06 -0500

    Instalar Pinia para el manejo de estados

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from "pinia";
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
